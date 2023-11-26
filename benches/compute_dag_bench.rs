4cf687dc7d53df2e22f7cd2ca6317a52eceda038
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-11-26 03:44:42 -0800

    bench: more predictable tree benchmarking
    Signed-off-by: Amin Yahyaabadi <aminyahyaabadi74@gmail.com>

--- benches/compute_dag_bench.rs
+++ benches/compute_dag_bench.rs
@@ -1,15 +1,20 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
 use dagrs::{log, Dag, DefaultTask, EnvVar, Input, LogLevel, Output, Task};
-use rand::distributions::{Distribution, Uniform};
-use std::{collections::HashSet, sync::Arc};
+use std::sync::Arc;
 
 fn calc(input: Input, env: Arc<EnvVar>) -> Output {
     let base = env.get::<usize>("base").unwrap();
     let mut sum = 2;
-    input
-        .get_iter()
-        .for_each(|i| sum += i.get::<usize>().unwrap() * base);
+    input.get_iter().for_each(|i| {
+        let mut i_usize = *i.get::<usize>().unwrap();
+        // avoid overflow
+        if i_usize > 1e6 as usize {
+            i_usize = 2;
+        }
+
+        sum += i_usize * base
+    });
     Output::new(sum)
 }
 
@@ -28,28 +33,17 @@ fn compute_dag(tasks: Vec<DefaultTask>) {
 fn compute_dag_bench(bencher: &mut Criterion) {
     let _initialized = log::init_logger(LogLevel::Off, None);
 
-    let mut tasks = (0..200usize)
+    let mut tasks = (0..50usize)
         .into_iter()
         .map(|i_task| DefaultTask::with_closure(&i_task.to_string(), calc))
         .collect::<Vec<_>>();
 
-    let mut rng = rand::thread_rng();
-
-    // consider up to 16 random predecessors from the previous 20 tasks.
+    // consider 8 dependency for each task (except first 20 tasks)
     for i_task in 20..tasks.len() {
-        let predecessors_id = (0..16usize)
+        let predecessors_id = ((i_task - 8)..i_task)
             .into_iter()
-            .filter_map(|_| {
-                let between = Uniform::from(i_task - 20..i_task);
-                let i_random = between.sample(&mut rng);
-
-                if i_random != i_task {
-                    Some(tasks[i_random].id())
-                } else {
-                    None
-                }
-            })
-            .collect::<HashSet<_>>();
+            .map(|i_dep| tasks[i_dep].id())
+            .collect::<Vec<_>>();
 
         tasks[i_task].set_predecessors_by_id(predecessors_id);
     }
@@ -60,7 +54,7 @@ fn compute_dag_bench(bencher: &mut Criterion) {
 criterion_group!(
   name = benches;
   config = {
-    let criterion = Criterion::default().sample_size(500);
+    let criterion = Criterion::default().sample_size(4000).noise_threshold(0.05);
     criterion
   };
   targets = compute_dag_bench
